bbbf0a79119b4c57eda7b6b6c9d628c94435c788
James Yang <yangguowei1@xiaomi.com>
2019-03-13 20:26:57 +0800

    [1.0.0] Add RecyclerView

--- CollegeReplacer/app/src/main/java/com/halo/collegereplacer/db/CRDatabase.kt
+++ CollegeReplacer/app/src/main/java/com/halo/collegereplacer/db/CRDatabase.kt
@@ -1,15 +1,31 @@
 package com.halo.collegereplacer.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 /**
  * Created by James on 2019/3/9.
  * Desc:
  */
-@Database(entities = arrayOf(User::class), version = 1)
+@Database(entities = [User::class], version = 1)
 abstract class CRDatabase: RoomDatabase() {
 
     abstract fun userDao(): UserDao
 
+    companion object {
+
+        @Volatile private var INSTANCE: CRDatabase? = null
+
+        fun getInstance(context: Context): CRDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context)
+            }
+
+        private fun buildDatabase(context: Context) =
+                Room.databaseBuilder(context.applicationContext,
+                    CRDatabase::class.java, "college_replace.db")
+                    .build()
+    }
 }
